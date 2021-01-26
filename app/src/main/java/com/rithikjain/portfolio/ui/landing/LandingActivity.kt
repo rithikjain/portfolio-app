5167f155be064156119760fd8b2aad217ab1d10b
Rithik Jain <rithik.jain3006@gmail.com>
2021-01-26 12:14:18 +0530

    📈 Added landing screen activity

--- app/src/main/java/com/rithikjain/portfolio/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/rithikjain/portfolio/ui/landing/LandingActivity.kt
@@ -1,11 +1,12 @@
-package com.rithikjain.portfolio
+package com.rithikjain.portfolio.ui.landing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rithikjain.portfolio.R
 
-class MainActivity : AppCompatActivity() {
+class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_landing)
     }
 }
