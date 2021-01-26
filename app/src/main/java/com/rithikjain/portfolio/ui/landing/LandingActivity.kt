95b7e6b7c1e05c7beb5fa7280bd0ec1aceae58a0
Rithik Jain <rithik.jain3006@gmail.com>
2021-01-26 13:32:43 +0530

    📈 Added typewriter font

--- app/src/main/java/com/rithikjain/portfolio/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/rithikjain/portfolio/ui/landing/LandingActivity.kt
@@ -3,10 +3,25 @@ package com.rithikjain.portfolio.ui.landing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rithikjain.portfolio.R
+import com.rithikjain.portfolio.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLandingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.introTextWriter.apply {
+            setWithMusic(false)
+            animateText("Hey! I'm Rithik Jain.")
+        }
+
+        binding.descriptionTextWriter.apply {
+            setWithMusic(false)
+            animateText("A Mobile Fullstack Dev :)")
+        }
     }
 }
